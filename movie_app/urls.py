060c19792f6e0dd419aac124b73a96dbd5999aab
EmirhanKuldashev <emirchankuldashew@gmail.com>
2024-05-07 19:00:08 +0600

    5 hw

---
+++ movie_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('movies/', views.movie_list),
+    path('movies/<int:id>/', views.movie_detail),
+
+    path('directors/', views.director_list),
+    path('directors/<int:id>/', views.director_detail),
+
+    path('reviews/', views.review_list),
+    path('reviews/<int:id>/', views.review_detail),
+]
